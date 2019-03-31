7dc2bc64b8ae441e6d8afc91a763828de879e170
LucianaPrudente <44330499+LucianaPrudente@users.noreply.github.com>
2019-03-31 17:34:59 -0300

    Create Challenges_Presentatios.py

---
+++ Challenges_Presentatios.py
@@ -0,0 +1,20 @@
+# Challenges -- Presentatios 
+""" ***   1   ***
+NOME = input ("Insira o seu nome: ")
+print("Olá", NOME, "! Prazer em conhecê-lo(a)")
+"""
+
+""" Challenges 2
+DIA = input ("Dia = ")
+MÊS = input ("Mês = ")
+ANO = input ("Ano = ")
+print(" Você nasceu no dia ", DIA, "de ", MÊS, "de", ANO, ". Correto?")
+"""
+
+a = input ("Insira o primeiro número: ")
+b = input ("Insira o segundo número: ")
+
+c = a+b
+print("O resultado da soma é: ", a+b)
+print("O resultado da soma é: ", (a++b))
+print("O resultado da soma é: ", c)
